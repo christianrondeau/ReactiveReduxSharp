4f63ffd7712b999174da0f3dd536f5aa7e62e325
Christian Rondeau <christian.rondeau@gmail.com>
2018-12-14 12:39:13 -0500

    Update the program example

--- ReduxExperiment/Program.cs
+++ ReduxExperiment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Newtonsoft.Json;
 using ReduxExperiment.Store;
 
@@ -9,15 +10,43 @@ namespace ReduxExperiment
 		public string[] Todos { get; set; } = new string[0];
 	}
 
+	public class AddTodo : IAction
+	{
+		public string Type { get; } = "ADD_TODO";
+		public string Payload;
+
+		public AddTodo(string name)
+		{
+			Payload = name;
+		}
+	}
+
+	public static class Reducers
+	{
+		public static State Reducer(State state, IAction action)
+		{
+			switch (action)
+			{
+				case AddTodo addTodo :
+					return new State
+					{
+						Todos = state.Todos.Concat(new[] {addTodo.Payload}).ToArray()
+					};
+				default:
+					return state;
+			}
+		}
+	}
+
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			var store = new ReduxStore<State>(new State());
-			store.Dispatch(new ReduxAction("ADD_TODO"));
+			var store = new ReduxStore<State>(new State(), Reducers.Reducer);
 			var subscription = store.Observable.Subscribe(
 				state => Console.WriteLine(JsonConvert.SerializeObject(state))
 			);
+			store.Dispatch(new AddTodo(args[0]));
 			Console.ReadLine();
 			subscription.Dispose();
 		}
